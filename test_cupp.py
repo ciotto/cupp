2a5304a71cc0e5b14c145497bbd62183eeb9e47e
ciotto <bianciottochristian@gmail.com>
2018-03-24 09:04:30 +0100

    implemented test_parser

--- test_cupp.py
+++ test_cupp.py
@@ -3,6 +3,7 @@
 import unittest
 from cupp3 import *
 
+
 class TestCupp3(unittest.TestCase):
     def setUp(self):
         read_config()
@@ -16,7 +17,69 @@ class TestCupp3(unittest.TestCase):
         self.assertTrue(os.path.isfile(os.path.join('dictionaries', 'french', 'dico.gz')))
 
     def test_parser(self):
-        pass
+        parser = get_parser()
+        with self.assertRaises(SystemExit):
+            parser.parse_args([])
+
+        with self.assertRaises(SystemExit):
+            parser.parse_args(['-i', '-v'])
+
+        with self.assertRaises(SystemExit):
+            parser.parse_args(['-i', '-a'])
+
+        with self.assertRaises(SystemExit):
+            parser.parse_args(['-i', '-l'])
+
+        args = parser.parse_args(['-a'])
+        self.assertFalse(args.quiet)
+        args = parser.parse_args(['-a', '--quiet'])
+        self.assertTrue(args.alecto)
+        self.assertFalse(args.download_wordlist)
+        self.assertFalse(args.improve)
+        self.assertFalse(args.interactive)
+        self.assertTrue(args.quiet)
+        self.assertFalse(args.version)
+
+        args = parser.parse_args(['-l'])
+        self.assertFalse(args.quiet)
+        args = parser.parse_args(['-l', '-q'])
+        self.assertFalse(args.alecto)
+        self.assertTrue(args.download_wordlist)
+        self.assertFalse(args.improve)
+        self.assertFalse(args.interactive)
+        self.assertTrue(args.quiet)
+        self.assertFalse(args.version)
+
+        args = parser.parse_args(['-a'])
+        self.assertFalse(args.quiet)
+        args = parser.parse_args(['-w', 'dictionary.txt', '--quiet'])
+        self.assertFalse(args.alecto)
+        self.assertFalse(args.download_wordlist)
+        self.assertTrue(args.improve)
+        self.assertFalse(args.interactive)
+        self.assertTrue(args.quiet)
+        self.assertFalse(args.version)
+
+        args = parser.parse_args(['-i'])
+        self.assertFalse(args.quiet)
+        args = parser.parse_args(['--interactive', '-q'])
+        self.assertFalse(args.alecto)
+        self.assertFalse(args.download_wordlist)
+        self.assertFalse(args.improve)
+        self.assertTrue(args.interactive)
+        self.assertTrue(args.quiet)
+        self.assertFalse(args.version)
+
+        args = parser.parse_args(['--version'])
+        self.assertFalse(args.quiet)
+        args = parser.parse_args(['-v', '--quiet'])
+        self.assertFalse(args.alecto)
+        self.assertFalse(args.download_wordlist)
+        self.assertFalse(args.improve)
+        self.assertFalse(args.interactive)
+        self.assertTrue(args.quiet)
+        self.assertTrue(args.version)
+
 
 if __name__ == '__main__':
     unittest.main()
