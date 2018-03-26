68133c083fe25b624bf03821387bd45c516f146c
ciotto <bianciottochristian@gmail.com>
2018-03-26 18:50:22 +0200

    add CuppMocker

--- test_cupp.py
+++ test_cupp.py
@@ -6,13 +6,72 @@ import cupp3
 from cupp3 import *
 
 
+class CuppMocker:
+    def __init__(self, reset=False):
+        self.reset = reset
+
+    def __enter__(self):
+        # Save values
+        self.config = cupp3.CONFIG
+        self.ftp_config = cupp3.FTP_CONFIG
+        self.leet_config = cupp3.LEET_CONFIG
+        self.verbose = cupp3.verbose
+
+        if self.reset:
+            # Reset values
+            cupp3.CONFIG = {}
+            cupp3.FTP_CONFIG = {}
+            cupp3.LEET_CONFIG = {}
+
+    def __exit__(self, *exc_info):
+        # Reset values
+        cupp3.CONFIG = self.config
+        cupp3.FTP_CONFIG = self.ftp_config
+        cupp3.LEET_CONFIG = self.leet_config
+        cupp3.verbose = self.verbose
+
+
 class TestCupp3(unittest.TestCase):
-    def setUp(self):
+    @classmethod
+    def setUpClass(cls):
         read_config()
+
+    def setUp(self):
+        self.mocker = CuppMocker()
+        self.mocker.__enter__()
+
+    def tearDown(self):
+        self.mocker.__exit__()
 
     @input_mocker.patch()
     def test_input(self):
         self.assertEqual(input_text(), 'y')
+
+    def test_cupp_mocker(self):
+        self.assertNotEqual(cupp3.CONFIG, {})
+        self.assertNotEqual(cupp3.FTP_CONFIG, {})
+        self.assertNotEqual(cupp3.LEET_CONFIG, {})
+        self.assertEqual(cupp3.verbose, False)
+
+        with CuppMocker(True):
+            self.assertEqual(cupp3.CONFIG, {})
+            self.assertEqual(cupp3.FTP_CONFIG, {})
+            self.assertEqual(cupp3.LEET_CONFIG, {})
+            self.assertEqual(cupp3.verbose, False)
+
+            cupp3.CONFIG = {'foo': 'bar'}
+            cupp3.FTP_CONFIG = {'foo': 'bar'}
+            cupp3.LEET_CONFIG = {'foo': 'bar'}
+            cupp3.verbose = True
+
+            self.assertEqual(cupp3.CONFIG, {'foo': 'bar'})
+            self.assertEqual(cupp3.FTP_CONFIG, {'foo': 'bar'})
+            self.assertEqual(cupp3.LEET_CONFIG, {'foo': 'bar'})
+            self.assertEqual(cupp3.verbose, True)
+        self.assertNotEqual(cupp3.CONFIG, {})
+        self.assertNotEqual(cupp3.FTP_CONFIG, {})
+        self.assertNotEqual(cupp3.LEET_CONFIG, {})
+        self.assertEqual(cupp3.verbose, False)
 
     def test_ftp_download(self):
         if not os.path.isdir('dictionaries'):
@@ -116,9 +175,9 @@ class TestCupp3(unittest.TestCase):
         self.assertEqual(colorize('Message', 32), '\033[1;32mMessage\033[1;m')
 
         self.assertEqual(info('Message\nmessage'), '\033[1;33m[i]\033[1;m Message\n    message')
-        cupp3.verbose = True
-        self.assertEqual(debug('Message\nmessage'), '\033[1;33m[v]\033[1;m Message\n    message')
-        cupp3.verbose = False
+        with CuppMocker():
+            cupp3.verbose = True
+            self.assertEqual(debug('Message\nmessage'), '\033[1;33m[v]\033[1;m Message\n    message')
         self.assertIsNone(debug('Message\nmessage'))
         self.assertEqual(success('Message\nmessage'), '\033[1;32m[+]\033[1;m Message\n    message')
         self.assertEqual(warning('Message\nmessage'), '\033[1;33m[!]\033[1;m Message\n    message')
